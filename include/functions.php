809754f590f39cfee6d9efd6e81711456e3cbfce
Hunter <thecryptohunter@protonmail.com>
2019-04-23 20:36:39 +0100

    Update function

--- include/functions.php
+++ include/functions.php
@@ -22,19 +22,12 @@ function crypto_rand($min,$max,$pedantic=True) {
 
 function CallAPI($url) {
 
-//  Initiate curl
-$ch = curl_init() ;
-// Will return the response, if false it print the response
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Set the url
-curl_setopt($ch, CURLOPT_URL,$url);
-// Execute
-$result = curl_exec($ch);
-// Closing
-curl_close($ch);
-// grab contents
-$return = file_get_contents($url);
-
+$ch = curl_init() ; //  Initiate curl
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_URL,$url); // Set the url
+$return = curl_exec($ch); // Execute
+curl_close($ch); // Closing
+$return = file_get_contents($url); // grab contents
 }
 
 ?>
