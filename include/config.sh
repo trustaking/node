e68f81c0296dbc2d4ba7e742445762abc0348c03
Hunter <thecryptohunter@protonmail.com>
2019-04-27 14:57:30 +0100

    update config

--- include/config.sh
+++ include/config.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-apiport="38222"; # "37222" <Main
+apiport="38222"; # "37222" <Main Redstone
+#apiport="42220"; # "42221" <Main X42
+#apiport="24335"; # "4335" <Main City
+#apiport="38222"; # "39222" <Main Impleum
