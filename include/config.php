2d174e480abadd009cf7172c67d36787e095c06d
Hunter <thecryptohunter@protonmail.com>
2019-05-16 22:01:07 +0100

    updates

--- include/config.php
+++ include/config.php
@@ -1,17 +1,17 @@
 <?php 
 // Coin specific variables
-$ticker='redstone';
-$api_port='38222'; 
+$ticker='';
+$api_port=''; 
+
+
+// Service specific variables.
+$service_desc='';
+$price= '';
+$redirectURL='';
+$email='';
 
 // BTCPayServer variables
-
 $encryt_pass  = 'YourTopSecretPassword';
-
-//TODO: Inject the redirectURL, Service, Price.
-$service_desc='12 months Trustaking service';
-$price= '15.00';
-$redirectURL='http://35.185.115.152/activate.php';
-$email='';
 
 // general variables
 $server_ip='localhost'; // '0.0.0.0' target server ip. [ex.] 10.0.0.15
