a1e8fb47fee1ee16dcde4f5345a7849f8cf19f5e
Hunter <thecryptohunter@protonmail.com>
2019-05-29 17:57:06 +0100

    Update

--- index.php
+++ index.php
@@ -39,11 +39,11 @@ if ( !is_array($get_stakinginfo) ) {
 
 if ($get_stakinginfo['enabled']>0) {
 $enabled = <<<EOD
-<a href="index.html" class="icon fa-circle" style="color:green">TRUSTAKING.COM</a>
+<a href="index.html" class="icon fa-circle" style="color:green"></a> TRUSTAKING.COM
 EOD;
 } else {
 $enabled = <<<EOD
-<a href="index.html" class="icon fa-circle" style="color:red">TRUSTAKING.COM</a>
+<a href="index.html" class="icon fa-circle" style="color:red"></a> TRUSTAKING.COM
 EOD;
 }
 
