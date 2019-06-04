dd5b85b109c21bd0b908674c23f0cb361cb33ae5
dirty-harry <41760565+dirty-harry@users.noreply.github.com>
2019-06-04 13:21:22 +0100

    Script output tweaks
    * cosmetic changes to make console output more readable
    * remove password display when adding funds
    * more prominent warning message

--- scripts/trustaking-cold-wallet-balance.ps1
+++ scripts/trustaking-cold-wallet-balance.ps1
@@ -5,10 +5,10 @@ $now = get-date
 
 ######## Get some information from the user about the wallet ############
 Clear-Host
-Write-Host "#######################################################################" -ForegroundColor Red
-Write-Host "########### TRUSTAKING.COM LOCAL WALLET - COLD STAKING SETUP ##########" -ForegroundColor Red
-Write-Host "#######################################################################`r`n" -ForegroundColor Red
-Write-Host "Use this script to get your current balance"
+Write-Host "#######################################################################" -ForegroundColor Red -BackgroundColor Gray
+Write-Host "############# TRUSTAKING.COM LOCAL WALLET - GET BALANCE ###############" -ForegroundColor Red -BackgroundColor Gray
+Write-Host "#######################################################################`r`n" -ForegroundColor Red -BackgroundColor Gray
+Write-Host "Use this script to get your current balance`n"
 
 $response = Read-Host -Prompt "Please enter the name of your wallet (default=MyColdWallet)" 
 if ($response) {
@@ -20,5 +20,5 @@ $WebResponse = Invoke-WebRequest "http://localhost:$apiport/api/Wallet/balance?W
 $result = $WebResponse.Content | ConvertFrom-Json
 $ColdStakingBalance=$result.balances.amountConfirmed / 100000000
 
-Write-Host "`r`nHere is the current confirmed balance at $now for wallet " $ColdWalletName ": " -NoNewline
-Write-Host $ColdStakingBalance -ForegroundColor Green
+Write-Host "`nHere is the current confirmed balance as at $now for wallet" $ColdWalletName": " -NoNewline
+Write-Host $ColdStakingBalance "`n" -ForegroundColor Green
--- scripts/trustaking-cold-wallet-setup.ps1
+++ scripts/trustaking-cold-wallet-setup.ps1
@@ -19,20 +19,20 @@ $ColdStakingTX=""
 
 ######## Get some information from the user about the wallet ############
 Clear-Host
-Write-Host "#######################################################################" -ForegroundColor Red
-Write-Host "########### TRUSTAKING.COM LOCAL WALLET - COLD STAKING SETUP ##########" -ForegroundColor Red
-Write-Host "#######################################################################`r`n" -ForegroundColor Red
-Write-Host "Firstly, let's create your cold wallet (which will hold the funds offline)"
+Write-Host "#######################################################################" -ForegroundColor Red -BackgroundColor Gray
+Write-Host "########### TRUSTAKING.COM LOCAL WALLET - COLD STAKING SETUP ##########" -ForegroundColor Red -BackgroundColor Gray
+Write-Host "#######################################################################`r`n" -ForegroundColor Red -BackgroundColor Gray
+Write-Host "Firstly, let's create your cold wallet (which will hold the funds offline)`n"
 $response = Read-Host -Prompt "Please enter your preferred Cold Wallet Name (default=MyColdWallet)" 
 if ($response) {
     $ColdWalletName = $response
 }
 
-$ColdWalletPassword = Read-Host -Prompt "Password for Cold Wallet"
-$ColdWalletPassphrase = Read-Host -Prompt "Passphrase for Cold Wallet" 
+$ColdWalletPassword = Read-Host -Prompt "`nPassword for Cold Wallet"
+$ColdWalletPassphrase = Read-Host -Prompt "`nPassphrase for Cold Wallet" 
 
 ##### Setup the Cold wallet ########
-Write-Host "* Creating your Cold wallet ... please wait."
+Write-Host "`n* Creating your Cold wallet ... please wait." -ForegroundColor DarkCyan
 
 $WebResponse = Invoke-WebRequest "http://localhost:$apiport/api/Wallet/mnemonic?language=english&wordCount=12"  ### grab a 12 word mneumonic
 $ColdWalletSecretWords = $WebResponse.Content -replace '"', ""
@@ -46,7 +46,7 @@ $json = @{
 } | ConvertTo-Json
 $response = Invoke-WebRequest "http://localhost:$apiport/api/Wallet/recover" -Method Post -Body $json -ContentType 'application/json-patch+json'
 LogWriter "Wallet recover result: $response"
-Write-Host "Cold Wallet Creation Result:" $response.StatusDescription -ForegroundColor Green
+Write-Host "* Cold Wallet Creation Result:" $response.StatusDescription -ForegroundColor DarkCyan
 
 ##### Grab the Cold wallet funding address ##########
 #Write-Host "* Getting the Initial funding address ... please wait."
@@ -55,8 +55,8 @@ $ColdWalletInitialFundingAddress = $WebResponse.Content -replace '"', ""
 LogWriter "ColdWalletInitialFundingAddress: $ColdWalletInitialFundingAddress"
 
 ##### Display info about the Cold wallet & funding details ######
-Write-Host "Send the funds you want to cold stake to this initial funding address on your Cold wallet then wait for the coins to confirm: "
-Write-Host $ColdWalletInitialFundingAddress
+Write-Host "`nSend the funds you want to cold stake to this initial funding address on your Cold wallet then wait for the coins to confirm: "
+Write-Host $ColdWalletInitialFundingAddress "`n"
 
 Write-Host -NoNewLine 'Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
@@ -77,14 +77,13 @@ $ColdWalletColdStakingColdAddress=$result.address
 LogWriter "ColdWalletColdStakingColdAddress: $ColdWalletColdStakingColdAddress"
 
 ##### Enter the Staking Address from Trustaking.com ######
-$HotWalletColdStakingHotAddress = Read-Host -Prompt "Enter your address from trustaking.com"
-$ColdStakingAmount = Read-Host -Prompt "Confirm how many coins you wish to start staking at trustaking.com"
+$HotWalletColdStakingHotAddress = Read-Host -Prompt "`n`nEnter your address from trustaking.com"
+$ColdStakingAmount = Read-Host -Prompt "`nConfirm how many coins you wish to start staking at trustaking.com"
 
 LogWriter "HotWalletColdStakingHotAddress: $HotWalletColdStakingHotAddress Amount: $ColdStakingAmount"
 
 ##### Prepare the cold staking tx ######
-Write-Host "* Preparing your cold staking transaction ... please wait."
-
+Write-Host "`n* Preparing your cold staking transaction ... please wait."
 $json = @{
     coldWalletAddress = $ColdWalletColdStakingColdAddress
     hotWalletAddress = $HotWalletColdStakingHotAddress
@@ -101,6 +100,7 @@ $ColdStakingTX = $result.transactionHex
 LogWriter $ColdStakingTX
 
 ##### Transmit the cold staking tx ######
+Write-Host "`n* Broadcasting your your cold staking funding transaction ... please wait." -ForegroundColor DarkCyan
 $json = @{
     hex = $ColdStakingTX
 } | ConvertTo-Json
@@ -110,8 +110,8 @@ $result = $response.Content | ConvertFrom-Json
 LogWriter $response.Content
 LogWriter "** End of Log **"
 
-Write-Host "Done.  The transaction ID is:" $result.transactionId.ToString()
-Write-Host "Here's your cold wallet details. These are you're responsibility trustaking.com has no way to recover this information - keep this information safe offline:"
+Write-Host "`nCold Staking Setup complete."
+Write-Host "`nHere's your cold wallet details:"
 Write-Host "Name      	:" $ColdWalletName
 Write-Host "Password  	:" $ColdWalletPassword
 Write-Host "Passphrase	:" $ColdWalletPassphrase
@@ -120,3 +120,5 @@ Write-Host "Funding address :" $ColdWalletInitialFundingAddress
 Write-Host "Cold address    :" $ColdWalletColdStakingColdAddress
 Write-Host "Hot address     :" $HotWalletColdStakingHotAddress
 Write-Host "Amount          :" $ColdStakingAmount
+Write-Host "Transaction ID  :" $result.transactionId.ToString()
+Write-Host "`n!!! Keeping your cold wallet details safe is your responsibility!`nTrustaking.com has no way to recover this information - keep this information safe offline.`n" -ForegroundColor Red -BackgroundColor Yellow
