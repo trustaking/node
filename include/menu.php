e7439c7f0cf573968b53e32491c10f51e9130cc9
Hunter <thecryptohunter@protonmail.com>
2019-07-03 12:33:08 +0100

    Update

--- include/menu.php
+++ include/menu.php
@@ -1,19 +1,20 @@
-    	<!-- Header -->
-			<header id="header" class="alt">
-				<h1><?php print $enabled;?></h1>
-		    		<nav id="nav">
-				        <ul>
-	    			        <li class="special">
-						    <a href="#menu" class="menuToggle"><span>Menu</span></a>
-						    <div id="menu">
-						        <ul>
-						            <?php print $message;?>
-						            <li><a href="index.php">Home</a></li>
-						            <li><a href="about.php">FAQ</a></li>
-						            <li><a href="https://btcpay.trustaking.com/apps/3ZLoV6ywKzV1JTBdx6DXEBWHXSxe/crowdfund">Tips</a></li>
-						        </ul>
-						    </div>
-						</li>
-				        </ul>
-			    	</nav>
-			</header>
+<?php include('include/node-check.php'); ?>
+<!-- Header -->
+<header id="header" class="alt">
+	<h1><?php print $enabled;?></h1>
+   		<nav id="nav">
+	        <ul>
+		        <li class="special">
+			    <a href="#menu" class="menuToggle"><span>Menu</span></a>
+			    <div id="menu">
+			        <ul>
+			            <?php print $message;?>
+			            <li><a href="index.php">Home</a></li>
+			            <li><a href="about.php">FAQ</a></li>
+			            <li><a href="https://btcpay.trustaking.com/apps/3ZLoV6ywKzV1JTBdx6DXEBWHXSxe/crowdfund">Tips</a></li>
+			        </ul>
+			    </div>
+			</li>
+	        </ul>
+    	</nav>
+</header>
--- index.php
+++ index.php
@@ -15,61 +15,6 @@ $_SESSION['Price'] = ceil(($price / $_SESSION['Days_Online']) * $days_remaining)
 if ($_SESSION['Price']>$price) {
 	$_SESSION['Price']=$price;
 }
-
-//Check if node is online before further checks
-$url = $scheme.'://'.$server_ip.':'.$api_port.'/api/Node/status' ;
-$check_server = $wallet->checkSite ($url);
-
-if ( $check_server == '' || empty($check_server) ) {
-$message = <<<EOD
-<li><a href=""class="icon fa-circle" style='color:red'>Node offline</a></li>
-EOD;
-$enabled = <<<EOD
-<a href="index.html" class="icon fa-circle" style="color:red">TRUSTAKING.COM </a>
-EOD;
-} else {
-
-// Grab balance
-$url = $scheme.'://'.$server_ip.':'.$api_port."/api/Wallet/balance?WalletName=$WalletName&AccountName=$AccountName";
-$get_balance = $wallet->CallAPI ($url); 
-	
-if ( !is_array($get_balance) ) {
-	die (' There was an error with your login parameters. Are your credentials correct?');
-} else {
-foreach($get_balance as $a => $b){
-	foreach($b as $c => $d){
-}
-$bal = $d['amountConfirmed']/100000000;
-}}
-
-// Get Node Staking Details
-$url = $scheme.'://'.$server_ip.':'.$api_port.'/api/Staking/getstakinginfo';
-$get_stakinginfo = $wallet->CallAPI ($url); 
-
-if ( !is_array($get_stakinginfo) ) {
-	die (' There was an error with your API parameters.');
-}
-
-if ($get_stakinginfo['enabled']>0) {
-$enabled = <<<EOD
-<a href="index.html" class="icon fa-circle" style="color:green">TRUSTAKING.COM</a>
-EOD;
-} else {
-$enabled = <<<EOD
-<a href="index.html" class="icon fa-circle" style="color:red">TRUSTAKING.COM </a>
-EOD;
-}
-
-if ($get_stakinginfo['staking']>0) {
-$message = <<<EOD
-<li><a href=""class="icon fa-circle" style='color:green'>Staking: $bal</a></li>
-EOD;
-} else {
-$message = <<<EOD
-<li><a href=""class="icon fa-circle" style='color:red'>Staking offline</a></li>
-EOD;
-}
-}
 ?>
 <?php include('include/header-recap.php'); ?>
 <?php include('include/menu.php'); ?>
