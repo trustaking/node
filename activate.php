264ed6b8189331d8020c1575b8418350350326fe
Hunter <thecryptohunter@protonmail.com>
2019-09-25 15:49:34 +0100

    Update http to https for scripts

--- activate.php
+++ activate.php
@@ -54,26 +54,26 @@ if ( $address == '' || empty($address) ) {
 			<p>Before you get started, open your local wallet and ensure it's fully synced.</p><br>
 			<?php if(!$isWin) { ?>
 				<p>Then open a terminal window and run the following script and follow the prompts:</p>
-				<pre><code>bash <( curl -s http://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-setup.sh )</code></pre>
+				<pre><code>bash <( curl -s https://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-setup.sh )</code></pre>
 				<p>Here is your hot wallet address when prompted: <pre><code><?php print $_SESSION['Address']; ?></code></pre></p>
 				<br/>
 				<p>Run this script at any time to see your cold staking balance:</p>
-				<pre><code>bash <( curl -s http://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-balance.sh )</code></pre>
+				<pre><code>bash <( curl -s https://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-balance.sh )</code></pre>
 				<p>If you need to add funds at a later date use this command:</p>
-				<pre><code>bash <( curl -s http://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-add-funds.sh )</code></pre>
+				<pre><code>bash <( curl -s https://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-add-funds.sh )</code></pre>
 				<p>And finally, when you want to withdraw your funds use this command:</p>
-				<pre><code>bash <( curl -s http://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-withdraw-funds.sh )</code></pre>
+				<pre><code>bash <( curl -s https://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-withdraw-funds.sh )</code></pre>
 			<?php } else { ?>
 				<p>Then open a Powershell window and run the following script and follow the prompts:</p>
-				<pre><code>iex ((New-Object System.Net.WebClient).DownloadString('http://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-setup.ps1'))</code></pre>
+				<pre><code>iex ((New-Object System.Net.WebClient).DownloadString('https://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-setup.ps1'))</code></pre>
 				<p>Here is your hot wallet address when prompted: <pre><code><?php print $_SESSION['Address']; ?></code></pre></p>
 				<br/>
 				<p>Run this script at any time to see your cold staking balance:</p>
-				<pre><code>iex ((New-Object System.Net.WebClient).DownloadString('http://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-balance.ps1'))</code></pre>
+				<pre><code>iex ((New-Object System.Net.WebClient).DownloadString('https://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-balance.ps1'))</code></pre>
 				<p>If you need to add funds at a later date use this command:</p>
-				<pre><code>iex ((New-Object System.Net.WebClient).DownloadString('http://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-add-funds.ps1'))</code></pre>
+				<pre><code>iex ((New-Object System.Net.WebClient).DownloadString('https://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-add-funds.ps1'))</code></pre>
 				<p>And finally, when you want to withdraw your funds use this command:</p>
-				<pre><code>iex ((New-Object System.Net.WebClient).DownloadString('http://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-withdraw-funds.ps1'))</code></pre>
+				<pre><code>iex ((New-Object System.Net.WebClient).DownloadString('https://<?php print $ticker; ?>.trustaking.com/scripts/trustaking-cold-wallet-withdraw-funds.ps1'))</code></pre>
 			<?php } ?>
 		</div>
 	</section>
