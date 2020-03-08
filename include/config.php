f0c0cd4017eab2c930e534573c834a27542d8dcd
Hunter <thecryptohunter@protonmail.com>
2020-03-08 18:55:18 +0000

    Fix config

--- include/config.php
+++ include/config.php
@@ -1,12 +1,12 @@
 <?php 
 // Coin specific variables will get injected
-$ticker='xds';
-$api_port='48334';
-$rpc_port='48333';
+$ticker='';
+$api_port=''; 
+$rpc_port='';
 
 // Service specific variables will get injected
-$redirectURL='https://xds.trustaking.com/activate.php';
-$ipnURL='https://xds.trustaking.com/IPNlogger.php';
+$redirectURL='';
+$ipnURL='';
 $email='';
 
 // general variables
@@ -14,9 +14,9 @@ $server_ip='localhost'; // '0.0.0.0' target server ip. [ex.] 10.0.0.15
 $scheme='http' ;// tcp protocol to access json on coin. [default]
 $AccountName='coldStakingHotAddresses' ; // special account for cold staking addresses
 $HotAccountName='account%200' ; // special account for hot addresses
-$api_ver='&Segwit=true'; // additional parameters for api call
-$coldstakeui='1'; // switch off the scripts
-$whitelist='1'; //is whitelisting enabled
-$payment='1'; // is payment enabled
-$exchange='1'; // is exchanged enabled
+$api_ver=''; // additional parameters for api call
+$coldstakeui=''; // switch off the scripts
+$whitelist=''; //is whitelisting enabled
+$payment=''; // is payment enabled (has to be used when whitelisting is enabled)
+$exchange=''; // is exchanged enabled
 ?>
