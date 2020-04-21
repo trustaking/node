6f0c7b081b132adb877b1f1ce29111d676fc3788
Hunter <thecryptohunter@protonmail.com>
2020-04-21 16:04:17 +0100

    add social media cards

--- include/header-recap.php
+++ include/header-recap.php
@@ -19,6 +19,15 @@ if (isset($bannerAd)) {
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+		<meta property="og:title" content="Trustaking.com">
+		<meta property="og:description" content="The trusted home of cold staking.">
+		<meta property="og:image" content="https://trustaking.com/images/thumbnail.png">
+		<meta property="og:image:width" content="1200">
+		<meta property="og:image:height" content="630">
+		<!--<meta property="og:url" content="">-->
+		<meta name="twitter:card" content="summary_large_image">
 		<script src='https://www.google.com/recaptcha/api.js?render=<?php echo $coinFunctions->keys['captcha_site_key']; ?>'></script>
         <script>
 			grecaptcha.ready(function () {
--- include/header.php
+++ include/header.php
@@ -10,6 +10,13 @@
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<meta property="og:title" content="Trustaking.com">
+		<meta property="og:description" content="The trusted home of cold staking.">
+		<meta property="og:image" content="https://trustaking.com/images/thumbnail.png">
+		<meta property="og:image:width" content="1200">
+		<meta property="og:image:height" content="630">
+		<!--<meta property="og:url" content="">-->
+		<meta name="twitter:card" content="summary_large_image">
 	</head>
 	<body class="landing is-preload">
 	<!-- Page Wrapper -->
