d43aa2629a294ad279a74e0ac7e1c1434e7ead0a
Hunter <thecryptohunter@protonmail.com>
2020-04-27 11:08:01 +0100

    add favicons to header

--- include/header.php
+++ include/header.php
@@ -10,6 +10,8 @@
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
 		<meta property="og:title" content="Trustaking.com">
 		<meta property="og:description" content="The trusted home of cold staking.">
 		<meta property="og:image" content="https://trustaking.com/images/thumbnail.png">
