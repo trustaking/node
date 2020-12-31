18dcca306930c5d1c0595358c5248f5938b71ce1
Hunter <thecryptohunter@protonmail.com>
2020-12-31 10:26:40 +0000

    Update terminology of cold staking address

--- activate.php
+++ activate.php
@@ -58,7 +58,7 @@ if ($coinFunctions->config['whitelist'] == '1') {
 				<h3>Thankyou for using Trustaking.com</h3><p>Please consider giving a donation <a href="https://donations.trustaking.com/"><b><u>here</b></u></a></p>
 			<?php }; ?>
 			<p>For a step by step tutorial, check out our how-to guide by visiting this <b><u><a href="how-to.php#trustaking">page</a></b></u></p>
-			<p>Here is your personal cold staking address: <pre><code><?php print $_SESSION['Address']; ?></code></pre></p>
+			<p>Here is your personal Trustaking address: <pre><code><?php print $_SESSION['Address']; ?></code></pre></p>
 			<p><b>IMPORTANT ... DO NOT SEND COINS DIRECTLY TO THIS ADDRESS</b> ... its for cold staking only </br></br>
 			If you're in any doubt ... ask for help in Discord.</p>
 		</div>
